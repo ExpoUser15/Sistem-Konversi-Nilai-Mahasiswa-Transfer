d350f9298ab0659e96321b5f453979e4fbacc800
ExpoUser15 <gideonmarchel@gmail.com>
2025-01-02 16:18:28 +0900

    perbaiki env

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,9 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize')
+require('dotenv').config();
 
-const URI = `mysql://${MYSQLUSER}:${MYSQLPASSWORD}@${MYSQLHOST}:${MYSQLPORT}/${MYSQLDATABASE}`;
-
-const sequelize = new Sequelize(URI);
+const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+});
 
 module.exports = sequelize;
